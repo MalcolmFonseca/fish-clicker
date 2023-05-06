da2bd0174117adf2b1d21d651e4d4e50243bdf36
MalcolmFonseca <mfonsec4@uwo.ca>
2023-05-06 11:37:26 -0400

    added text to shop

--- main.py
+++ main.py
@@ -18,15 +18,20 @@ screen.fill(light_water_color)
 main_shop = shop.Shop(window_size)
 
 #create shop buttons
-seaweed_btn = shopButton.ShopButton("Seaweed",15,'Assets\seaweed.png')
+seaweed_btn = shopButton.ShopButton("Seaweed",15,'Assets/seaweed.png')
+seaslug_btn = shopButton.ShopButton("Seaslug",100,'Assets/seaslug.png')
+crab_btn = shopButton.ShopButton("Crab",1_100,'Assets/crab.png')
+angelfish_btn = shopButton.ShopButton("Angelfish",12_000,'Assets/angelfish.png')
+clownfish_btn = shopButton.ShopButton("Clownfish",130_000,'Assets/clownfish.png')
+squid_btn = shopButton.ShopButton("Squid",1_400_000,'Assets/squid.png')
 
 #add buttons to main_shop
 main_shop.all_buttons.append(seaweed_btn)
-main_shop.all_buttons.append(seaweed_btn)
-main_shop.all_buttons.append(seaweed_btn)
-main_shop.all_buttons.append(seaweed_btn)
-main_shop.all_buttons.append(seaweed_btn)
-main_shop.all_buttons.append(seaweed_btn)
+main_shop.all_buttons.append(seaslug_btn)
+main_shop.all_buttons.append(crab_btn)
+main_shop.all_buttons.append(angelfish_btn)
+main_shop.all_buttons.append(clownfish_btn)
+main_shop.all_buttons.append(squid_btn)
 main_shop.all_buttons.append(seaweed_btn)
 
 main_shop.current_buttons = main_shop.all_buttons
@@ -40,6 +45,24 @@ bottom_arrow = pygame.transform.rotate(top_arrow,180)
 bottom_arrow_rect = bottom_arrow.get_rect()
 bottom_arrow_rect.center = [seaweed_btn.button_rect.centerx,window_size[1]-35]
 
+#create text for shop
+title_font = pygame.font.Font('Assets/Kamalla.ttf',50)
+shop_title_text = title_font.render('Shop',True,(0,0,0))
+shop_title_text_rect = shop_title_text.get_rect()
+shop_title_text_rect.left = seaweed_btn.button_rect.left
+shop_title_text_rect.top = 5
+
+#create text for 
+score_font = pygame.font.Font('Assets/Kamalla.ttf',80)
+score_text = score_font.render('0',True,(0,0,0))
+score_text_rect = score_text.get_rect()
+score_text_rect.centerx = window_size[0]/3
+
+#function to update score
+def update_score():
+    pass
+
+#render method
 def render():
     #render shop box
     pygame.draw.rect(screen,brown_color,main_shop.shop_rect)
@@ -51,10 +74,16 @@ def render():
         position += 1
         pygame.draw.rect(screen,sand_color,button.button_rect)
         screen.blit(button.image,button.image_rect)
+        screen.blit(button.name_text,button.name_text_rect)
+        screen.blit(button.cost_text,button.cost_text_rect)
 
     #render arrows
     screen.blit(top_arrow,top_arrow_rect)
     screen.blit(bottom_arrow,bottom_arrow_rect)
+
+    #render shop title and score text
+    screen.blit(shop_title_text,shop_title_text_rect)
+    screen.blit(score_text,score_text_rect)
 
     pygame.display.flip()
 
--- shopButton.py
+++ shopButton.py
@@ -1,6 +1,6 @@
 import pygame
 
-class ShopButton():
+class ShopButton():   
     def __init__(self,name,cost,image_path):
         self.button_rect = pygame.Rect((1920/3)*2+15,15,(1920/3)-30,120)
         self.name = name
@@ -8,9 +8,26 @@ class ShopButton():
         self.image = pygame.image.load(image_path)
         self.image_rect = self.image.get_rect()
 
+        #create text
+        kamalla_font = pygame.font.Font('Assets/Kamalla.ttf',70)
+        self.name_text = kamalla_font.render(f'{self.name}',True,(0,0,0))
+        self.name_text_rect = self.name_text.get_rect()
+
+        self.cost_text = kamalla_font.render(f'{self.cost}',True,(0,0,0))
+        self.cost_text_rect = self.cost_text.get_rect()
+
     def position(self, position_num):
         #15 is size of gap 120 is height of button
+
+        #position box
         top = position_num*15 + (position_num-1)*120 + 45
         self.button_rect.top = top
+        #position image
         self.image_rect.left = (1920/3)*2+15
-        self.image_rect.top = top
+        self.image_rect.top = top + 10
+        #position name text
+        self.name_text_rect.left = self.image_rect.right + 15
+        self.name_text_rect.centery = self.button_rect.centery
+        #position cost text
+        self.cost_text_rect.right = self.button_rect.right - 15
+        self.cost_text_rect.centery = self.button_rect.centery
